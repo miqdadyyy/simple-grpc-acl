660eb3df70fcfdc7dcbc9e731af64e51747e6af7
Miqdad Farcha <miqdad.farcha@gmail.com>
2021-09-14 12:38:45 +0700

    Add grpc function

--- pkg/grpc_authorization.go
+++ pkg/grpc_authorization.go
@@ -24,7 +24,12 @@ func (g *GrpcServerAuthorization) Authorize(ctx context.Context, permission *Per
 	return nil
 }
 
-func (g *GrpcServerAuthorization) AuthorizeWithTeam(ctx context.Context, permission *Permission, teamId string, action ...string) {
-	//model := g.GetModelFromContext(ctx)
+func (g *GrpcServerAuthorization) AuthorizeWithTeam(ctx context.Context, permission *Permission, teamId string, action ...string) error {
+	model := g.GetModelFromContext(ctx)
+	allowed := g.acl.CheckPermissionInModelWithTeam(permission, model, teamId, action...)
+	if !allowed {
+		return errors.New("Current user doesn't has permission in this resource")
+	}
 
+	return nil
 }
